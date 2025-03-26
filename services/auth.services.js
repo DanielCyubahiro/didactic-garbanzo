5bc357dbba9fcc94269e01759fe8e55622f3c108
Daniel <cyubahirodaniel@gmail.com>
2025-03-26 10:34:43 +0100

    Refactor auth services

--- services/auth.services.js
+++ services/auth.services.js
@@ -1,14 +1,8 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const hashPassword = async (password) => {
-  return await bcrypt.hash(password, 10);
-};
-
-const comparePassword = async (password, hashedPassword) => {
-  return await bcrypt.compare(password, hashedPassword);
-};
-
+const hashPassword = async (password) => await bcrypt.hash(password, 10);
+const comparePassword = async (password, hashedPassword) => await bcrypt.compare(password, hashedPassword);
 const generateToken = async (user) => jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES});
 const generateRefreshToken = async (user) => jwt.sign({id: user.id}, process.env.JWT_REFRESH_SECRET, {expiresIn: process.env.JWT_REFRESH_EXPIRES});
 
