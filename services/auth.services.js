f1a8f3800b12c9d6fbe01d85af66acc80c408251
Daniel <cyubahirodaniel@gmail.com>
2025-03-26 12:54:46 +0100

    Update naming from generateToken to generateToken

--- services/auth.services.js
+++ services/auth.services.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const hashPassword = async (password) => await bcrypt.hash(password, 10);
 const comparePassword = async (password, hashedPassword) => await bcrypt.compare(password, hashedPassword);
-const generateToken = async (user) => jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES});
+const generateAccessToken = async (user) => jwt.sign({id: user.id, email: user.email}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES});
 const generateRefreshToken = async (user) => jwt.sign({id: user.id}, process.env.JWT_REFRESH_SECRET, {expiresIn: process.env.JWT_REFRESH_EXPIRES});
 
-module.exports = {hashPassword, comparePassword, generateToken, generateRefreshToken};
+module.exports = {hashPassword, comparePassword, generateAccessToken, generateRefreshToken};
