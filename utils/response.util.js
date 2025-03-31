5239e4a04b2907f0823f5a09e5a2ff0e22f6ee81
Daniel <cyubahirodaniel@gmail.com>
2025-03-31 18:25:16 +0200

    Add custom API response and error display

---
+++ utils/response.util.js
@@ -0,0 +1,14 @@
+class ApiResponse {
+  /**
+   * @param {number} statusCode
+   * @param {Array} data
+   * @param {string} message
+   */
+  constructor(statusCode, data, message) {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+module.exports = ApiResponse;
